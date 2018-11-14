41b3cc7391462ffd6c5267b727a0c63b0a728bde
Arongil <lakernewhouse@gmail.com>
2018-11-14 13:25:16 -0800

    Update sketch.js

--- sound/sketch.js
+++ sound/sketch.js
@@ -252,19 +252,19 @@ function binomial(n, k) {
 
 var p5 = new p5();
 var shushes = [
-	p5.loadSound("shush1.mp3"),
-	p5.loadSound("shush2.mp3"),
-	p5.loadSound("shush3.mp3"),
-	p5.loadSound("shush4.mp3")
+	p5.loadSound("audio/shush1.mp3"),
+	p5.loadSound("audio/shush2.mp3"),
+	p5.loadSound("audio/shush3.mp3"),
+	p5.loadSound("audio/shush4.mp3")
 ];
 var bells = [
-	p5.loadSound("bell.mp3")
+	p5.loadSound("audio/bell.mp3")
 ];
 var warnings = [
-	p5.loadSound("megan_warning.mp3")
+	p5.loadSound("audio/megan_warning.mp3")
 ];
 var classtimes = [
-	p5.loadSound("classtime.mp3")
+	p5.loadSound("audio/classtime.mp3")
 ];
 
 function setup() {
