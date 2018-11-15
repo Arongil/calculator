212d9b55d0151a67cd6138f1fc6c10f727e53ca6
Arongil <lakernewhouse@gmail.com>
2018-11-14 19:01:30 -0900

    Update audio.js

--- sound/audio.js
+++ sound/audio.js
@@ -1,17 +1,17 @@
-var p5 = new p5();
+var p5Loader = new p5();
 var shushes = [
-	p5.loadSound("audio/shush1.mp3"),
-	p5.loadSound("audio/shush2.mp3"),
-	p5.loadSound("audio/shush3.mp3"),
-	p5.loadSound("audio/shush4.mp3")
+	p5Loader.loadSound("audio/shush1.mp3"),
+	p5Loader.loadSound("audio/shush2.mp3"),
+	p5Loader.loadSound("audio/shush3.mp3"),
+	p5Loader.loadSound("audio/shush4.mp3")
 ];
 var bells = [
-	p5.loadSound("audio/bell.mp3")
+	p5Loader.loadSound("audio/bell.mp3")
 ];
 var warnings = [
-	p5.loadSound("audio/megan_warning.mp3"),
-	p5.loadSound("audio/gabrielle_warning.mp3")	
+	p5Loader.loadSound("audio/megan_warning.mp3"),
+	p5Loader.loadSound("audio/gabrielle_warning.mp3")	
 ];
 var classtimes = [
-	p5.loadSound("audio/classtime.mp3")
+	p5Loader.loadSound("audio/classtime.mp3")
 ];
